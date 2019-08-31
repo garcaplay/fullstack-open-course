50502528b56ef34a599ec42c0d73ccd594e49c8d
Silvia García Cacho <silvia@garca.info>
2019-08-31 14:20:58 +0200

    step1 Define the react hooks

---
+++ src/unicafe/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Unicafe = () => {
+    
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+    
+    return(
+        <div>
+            code here
+        </div>
+    )
+}
+
+export default Unicafe;
