f8f7c90cab64fd4e3649e6fe812f3a61d35f33d2
Silvia García Cacho <silvia@garca.info>
2019-09-08 18:34:35 +0200

    step3 Insert the most voted anecdote

--- src/anecdotes/index.js
+++ src/anecdotes/index.js
@@ -5,7 +5,7 @@ const anecdotesArray = [
    { text: 'Adding manpower to a late software project makes it later!', vote:0},
     {text:'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.', vote:0},
     {text: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', vote: 0},
-    {text:'Premature optimization is the root of all evil.',vote:0},
+    {text:'Premature optimization is the root of all evil.',vote:12},
     { text:'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.', vote:0}
   ]
 
@@ -23,17 +23,25 @@ const Anecdotes = () => {
         anecdotesArrayCopy[selected].vote += 1; 
         setAnecdotes(anecdotesArrayCopy);
     }
-
+const anecdoteWithMostVotes = anecdotes.reduce((anecdote1, anecdote2) => anecdote1.vote > anecdote2.vote ? anecdote1 : anecdote2);
     return(
         <Fragment>
-        <div>
-           {anecdotes[selected].text} 
-        </div>
-        <div>Has {anecdotes[selected].vote} votes</div>
-        <div>
-            <button onClick={handleAnecdoteVote}>Vote anecdote</button>
-            <button onClick={getRandomAnecdote}>Next anecdote</button>
-        </div>
+            <div>
+                <h2>Anecdote of the day</h2>
+                <p>
+                {anecdotes[selected].text} 
+                </p>
+                <p>Has {anecdotes[selected].vote} votes</p>
+            </div>
+            <div>
+                <button onClick={handleAnecdoteVote}>Vote anecdote</button>
+                <button onClick={getRandomAnecdote}>Next anecdote</button>
+            </div>
+            <div>
+                <h2>Anecdote with most votes</h2>
+                <p>{anecdoteWithMostVotes.text}</p>
+                <p>Has {anecdoteWithMostVotes.vote} votes</p>
+            </div>
         </Fragment>
     )
 }
