edf81050b5e10a081c6bc4a9deaf930b6e7f7e57
Silvia García Cacho <silvia@garca.info>
2019-09-08 18:19:56 +0200

    step2 Insert a vote function

--- src/anecdotes/index.js
+++ src/anecdotes/index.js
@@ -1,30 +1,37 @@
 import React, {useState, Fragment} from 'react';
 
 const anecdotesArray = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+   {text: 'If it hurts, do it more often', vote:0},
+   { text: 'Adding manpower to a late software project makes it later!', vote:0},
+    {text:'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.', vote:0},
+    {text: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', vote: 0},
+    {text:'Premature optimization is the root of all evil.',vote:0},
+    { text:'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.', vote:0}
   ]
 
 
 const Anecdotes = () => {
     
     const [selected, setSelected] = useState(0);
-    const [anecdotes, setAnecdotes] = useState(anecdotesArray)
+    const [anecdotes, setAnecdotes] = useState(anecdotesArray);
     const getRandomAnecdote = ()=>{
         const randomNumber = Math.random()*(anecdotesArray.length - 0) + 0;
         setSelected(Math.floor(randomNumber));
+    }
+    const handleAnecdoteVote =()=>{
+        const anecdotesArrayCopy = [...anecdotes];
+        anecdotesArrayCopy[selected].vote += 1; 
+        setAnecdotes(anecdotesArrayCopy);
     }
 
     return(
         <Fragment>
         <div>
-           {anecdotes[selected]} 
+           {anecdotes[selected].text} 
         </div>
+        <div>Has {anecdotes[selected].vote} votes</div>
         <div>
+            <button onClick={handleAnecdoteVote}>Vote anecdote</button>
             <button onClick={getRandomAnecdote}>Next anecdote</button>
         </div>
         </Fragment>
