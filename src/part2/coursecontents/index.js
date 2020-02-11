2b4e13e1c78fe9a63dd4f5b3c31aa0b095e90e65
Silvia García Cacho <silvia@garca.info>
2020-02-11 11:06:41 +0100

    Change name on folder

--- src/part2/coursecontents/index.js
+++ src/part2/coursecontents/index.js
@@ -2,55 +2,55 @@ import React from 'react';
 import Subjects from './components/Subjects';
 
 const CourseContents = () => {
-    const subjects = [
+  const subjects = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
         {
-            name: 'Half Stack application development',
-            id: 1,
-            parts: [
-              {
-                name: 'Fundamentals of React',
-                exercises: 10,
-                id: 1
-              },
-              {
-                name: 'Using props to pass data',
-                exercises: 7,
-                id: 2
-              },
-              {
-                name: 'State of a component',
-                exercises: 14,
-                id: 3
-              },
-              {
-                name: 'Redux',
-                exercises: 11,
-                id: 4
-              }
-            ]
-        }, 
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
         {
-            name: 'Node.js',
-            id: 2,
-            parts: [
-              {
-                name: 'Routing',
-                exercises: 3,
-                id: 1
-              },
-              {
-                name: 'Middlewares',
-                exercises: 7,
-                id: 2
-              }
-            ]
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
         }
-    ]
-    return (
-        <div>
-            <Subjects subjects={subjects}/>
-        </div>
-    )
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  return (
+    <div>
+      <Subjects subjects={subjects} />
+    </div>
+  )
 }
 
 
